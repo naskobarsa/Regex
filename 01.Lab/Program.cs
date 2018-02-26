4bd5e9b8d6656d0c8e8b6f0e98c565846194d289
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-02-26 13:34:44 +0200

    Add files via upload

---
+++ 01.Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+      
+        string input = @"Ivan IvanovE, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan	Ivanov
+";      string replacement = "nn";
+        Regex regex = new Regex(@"[A-Z]+([a-z])");
+        var options = regex.Matches(input);
+        string newregex = regex.Replace(input, replacement);
+        foreach (Match item in options)
+        {
+
+            Console.WriteLine(item.Groups[1].Index);
+        }
+
+        Console.WriteLine(newregex);
+    }
+}
