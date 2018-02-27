4ca02bcd8a23ae2d26c9da1fa78e465259289a98
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-02-27 18:56:18 +0200

    Add files via upload

---
+++ 02.Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var phones = Console.ReadLine();
+
+            var pattern = @"\+359+(-| ?)2(\1)[\d]{3}(\1)[\d]{4}\b";
+
+            var phonesMatches = Regex.Matches(phones, pattern);
+
+            var matchedPhones = phonesMatches.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(", ",matchedPhones));
+        }
+    }
+}
