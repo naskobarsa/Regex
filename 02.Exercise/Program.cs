43f211d62f26c9f33cfe9d80c9b556f1998cccf1
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-02-27 18:54:59 +0200

    Add files via upload

---
+++ 02.Exercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main()  // 80/100
+    {
+        string searched = Console.ReadLine();
+        string input = Console.ReadLine();
+        //string pattern = "([A-Za-z0-9 -]+ " + searched + " [A-Za-z0-9 -]+[!|.|?])";
+        string pattern = $@"[a-zA-Z0-9,\s]+ {searched} [a-zA-Z0-9,\s\-]+";
+
+        Regex regex = new Regex(pattern);
+        MatchCollection matches = regex.Matches(input);
+
+        foreach (var match in matches)
+        {
+           
+
+            Console.WriteLine(match.ToString().TrimStart(' '));
+        }
+    }
+}
