c8ebc248f5c99ad6da300063d28aa2a81b9e0214
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-02-27 18:57:52 +0200

    Add files via upload

---
+++ 03.Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var someString = Console.ReadLine();
+
+            var pattern = @"\b(0x)*[0-9A-F]+\b";
+
+            var numbers = Regex.Matches(someString, pattern);
+
+            var matchedNumbers = numbers.Cast<Match>().Select(a => a.Value.Trim()).ToArray();
+
+            Console.WriteLine(string.Join(" ", matchedNumbers));
+        }
+    }
+}
