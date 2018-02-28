7b40c859eddb390b8225e9e3f74ec14e79f71232
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-02-28 10:51:38 +0200

    Add files via upload

---
+++ 07.Exercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main() // 66/100
+    {
+        char[] input = Console.ReadLine().ToCharArray();
+
+        string output = String.Empty;
+        int counter = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            bool digit = char.IsDigit(input[i]);
+
+            if (!digit && counter != 0)
+            {
+                output += input[i];
+                output += input[i];
+                counter = 0;
+                continue;
+            }
+            if (!digit)
+            {
+                output += input[i];
+                continue;
+
+            }
+            counter++;
+
+        }
+        Console.WriteLine("{0}", string.Join("", output));
+    }
+}
