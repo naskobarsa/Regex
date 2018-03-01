a3fae9180aa83552c92c8dbbdc60028a5ac9a93b
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-03-01 10:16:00 +0200

    Add files via upload

---
+++ 04.Lab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dateString = Console.ReadLine();
+
+            var pattern = @"\b(?<day>[0-9]{2})([-.\/])(?<month>[A-Z]{1}[a-z]{2})\1(?<year>[0-9]{4})\b";
+
+            var dates = Regex.Matches(dateString, pattern);
+
+            foreach (Match item in dates)
+            {
+                var day = item.Groups["day"].Value;
+                var month = item.Groups["month"].Value;
+                var year = item.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
