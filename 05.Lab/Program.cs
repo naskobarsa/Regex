3f2e444ee9fc7557bd535046487809f9b031f9c0
Atanas Atanasov <32620407+naskobarsa@users.noreply.github.com>
2018-03-01 10:17:19 +0200

    Add files via upload

---
+++ 05.Lab/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _02.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine();
+
+            var pattern = @"(^|(?<=\s))-?[0-9]+(\.\d+)?($|(?=\s))";
+
+            var posNumbers = Regex.Matches(numbers, pattern);
+
+            foreach (Match item in posNumbers)
+            {
+               
+
+                Console.Write(item.Value + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 05.More Exercises/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program
+{
+    static void Main() // 100/100
+    {
+        string inputLine = Console.ReadLine();
+        string pattern = @"(\d+)([A-Za-z])";
+        
+
+        MatchCollection replacer = Regex.Matches(inputLine, pattern);
+
+        foreach (Match find in replacer)
+        {
+            inputLine = Regex.Replace(inputLine, find.Groups[1].ToString(), find.Groups[2].Value.ToString());
+        }
+        Console.WriteLine(inputLine);
+    }
+}
